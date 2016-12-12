bab1e982a0989355679b74f544cef8b556f379df
Maxence Groine <maxence.groine@airinov.fr>
2016-12-12 18:24:08 +0100

    Solved Set 1 / Challenge 8

---
+++ Set_1/eight.py
@@ -0,0 +1,21 @@
+from Crypto.Cipher import AES
+import binascii
+
+def get_16_bytes_blocks_frequency(ciphertext):
+    blocks = [ciphertext[i*16:(i+1)*16] for i in range(len(ciphertext) / 16)]
+    return len(set(blocks))
+
+if __name__ == '__main__':
+    with open("./data/8.txt", "r") as f:
+        cipher_texts = [line.strip() for line in f]
+
+    minScore = 100
+
+    for ciphertext in cipher_texts:
+        curScore = get_16_bytes_blocks_frequency(ciphertext)
+        if curScore < minScore:
+            winner = ciphertext
+            minScore = curScore
+
+    print("The best candidate is {}... with {} repeating blocks".format(winner[:16],
+                                                                     len(winner)/16 - minScore))
