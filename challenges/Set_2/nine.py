bda9467ad13070f8516278cfc5dd430ad426b849
Maxence Groine <maxence.groine@airinov.fr>
2016-12-13 13:37:52 +0100

    Add `pad_string` function to nine.py

--- challenges/Set_2/nine.py
+++ challenges/Set_2/nine.py
@@ -1,9 +1,15 @@
 def pad_bytes(input_bytes, padding_byte, block_size):
-	while len(input_bytes) % block_size != 0:
-		input_bytes += padding_byte
-	return input_bytes
+    while len(input_bytes) % block_size != 0:
+        input_bytes += padding_byte
+    return input_bytes
+
+def pad_string(input_string, padding_byte, block_size):
+    while len(input_string) % block_size != 0:
+        input_string += chr(padding_byte)
+    return input_string
 
 if __name__ == "__main__":
-	input_str = "YELLOW SUBMARINE"
-	input_bytes = input_str.encode('utf-8')
-	print(pad_bytes(input_bytes, bytes.fromhex("04"), 20))
+    input_str = "YELLOW SUBMARINE"
+    input_bytes = input_str.encode('utf-8')
+    print(pad_bytes(input_bytes, bytes.fromhex("04"), 20))
+    print(pad_string(input_str, 4, 20))
